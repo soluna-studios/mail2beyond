6764abadbbb349e43a452608735210751e57ee9d
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-08-08 11:44:04 -0600

    chore: fixed linting issues in setup.py

--- mail2chat/tools.py
+++ mail2chat/tools.py
@@ -1,9 +1,12 @@
-import logging
-import ssl
-import mail2chat
 import inspect
+import logging
 import pathlib
+import ssl
+
 from OpenSSL import crypto
+
+from . import connectors
+from . import framework
 
 
 def get_connectors_from_dict(config: dict):
@@ -14,42 +17,42 @@ def get_connectors_from_dict(config: dict):
     """
     # Create a list to store the created connector objects
     valid_connectors = []
-    available_connectors = dict(inspect.getmembers(mail2chat.connectors, inspect.ismodule))
+    available_connectors = dict(inspect.getmembers(connectors, inspect.ismodule))
 
     # Require connectors config to be defined
     if "connectors" not in config.keys():
-        raise mail2chat.framework.Error("'connectors' value is required")
+        raise framework.Error("'connectors' value is required")
 
     # Require connectors config to be a list
     if type(config.get("connectors")) != list:
-        raise mail2chat.framework.Error("'connectors' value must be type list")
+        raise framework.Error("'connectors' value must be type list")
 
     # Require at least one connector
     if len(config.get("connectors")) == 0:
-        raise mail2chat.framework.Error("at least one 'connectors' item is required")
+        raise framework.Error("at least one 'connectors' item is required")
 
     # Loop through each connector in the configuration
     for connector in config.get("connectors"):
         # Require connector definition to be a dict
         if type(connector) != dict:
-            raise mail2chat.framework.Error("'connectors' items must be type dict")
+            raise framework.Error("'connectors' items must be type dict")
 
         # Require connector name to be defined
         if "name" not in connector.keys():
-            raise mail2chat.framework.Error("'connectors' items must contain 'name' value")
+            raise framework.Error("'connectors' items must contain 'name' value")
 
         # Require connector names to be unique
         for obj in valid_connectors:
             if connector["name"] == obj.name:
-                raise mail2chat.framework.Error(f"multiple 'connectors' items assigned name '{obj.name}'")
+                raise framework.Error(f"multiple 'connectors' items assigned name '{obj.name}'")
 
         # Require connector type to be defined
         if "connector" not in connector.keys():
-            raise mail2chat.framework.Error("'connectors' configuration items must contain 'connector' value")
+            raise framework.Error("'connectors' configuration items must contain 'connector' value")
 
         # Require connector type to be known
         if connector["connector"] not in available_connectors.keys():
-            raise mail2chat.framework.Error("'connectors' configuration items must contain 'connector' value")
+            raise framework.Error("'connectors' configuration items must contain 'connector' value")
         else:
             connector_module = available_connectors.get(connector["connector"])
 
@@ -89,38 +92,38 @@ def get_mappings_from_dict(config: dict):
 
     # Require mappings config to be defined
     if "mappings" not in config.keys():
-        raise mail2chat.framework.Error("'mappings' value is required")
+        raise framework.Error("'mappings' value is required")
 
     # Require mappings config to be a list
     if type(config.get("mappings")) != list:
-        raise mail2chat.framework.Error("'mappings' value must be type list")
+        raise framework.Error("'mappings' value must be type list")
 
     # Require at least one mapping
     if len(config.get("mappings")) == 0:
-        raise mail2chat.framework.Error("at least one 'mappings' item is required")
+        raise framework.Error("at least one 'mappings' item is required")
 
     # Loop through each mapping in the configuration
     for mapping in config.get("mappings"):
         # Require mapping definition to be a dict
         if type(mapping) != dict:
-            raise mail2chat.framework.Error("'mappings' items must be type dict")
+            raise framework.Error("'mappings' items must be type dict")
 
         # Require mapping connector to be defined
         if "connector" not in mapping.keys():
-            raise mail2chat.framework.Error("'mappings' items must contain 'connector' value")
+            raise framework.Error("'mappings' items must contain 'connector' value")
 
         # Get this mapping's connector
         connector = get_connector_by_name(mapping.get("connector"), connectors)
 
         # Require this mapping's connector to exist
         if not connector:
-            raise mail2chat.framework.Error(f"'mappings' item references undefined connector '{mapping['connector']}'")
+            raise framework.Error(f"'mappings' item references undefined connector '{mapping['connector']}'")
 
         # Replace the mapping's connector item with the actual object so it can be passed in with kwargs below
         mapping["connector"] = connector
 
         # Add this mapping, it is valid
-        mapping_obj = mail2chat.framework.Mapping(**mapping)
+        mapping_obj = framework.Mapping(**mapping)
         valid_mappings.append(mapping_obj)
 
     # Return our connector objects
@@ -152,15 +155,15 @@ def get_listeners_from_dict(config: dict, log_level: int = logging.NOTSET):
 
     # Require listeners config to be defined.
     if "listeners" not in config.keys():
-        raise mail2chat.framework.Error("'listeners' value is required")
+        raise framework.Error("'listeners' value is required")
 
     # Require listeners config to be a list
     if type(config.get("listeners")) != list:
-        raise mail2chat.framework.Error("'listeners' value must be type list")
+        raise framework.Error("'listeners' value must be type list")
 
     # Require at least one listener
     if len(config.get("listeners")) == 0:
-        raise mail2chat.framework.Error("at least one 'listeners' item is required")
+        raise framework.Error("at least one 'listeners' item is required")
 
     # Loop through each listener and ensure it has a valid configuration.
     for listener in config.get("listeners"):
@@ -170,19 +173,19 @@ def get_listeners_from_dict(config: dict, log_level: int = logging.NOTSET):
 
         # Require listener definition to be a dict
         if type(listener) != dict:
-            raise mail2chat.framework.Error("'listeners' items must be type dict")
+            raise framework.Error("'listeners' items must be type dict")
 
         # Ensure listener has an address and port set. The Listener object will validate further upon creation.
         if "address" not in listener.keys():
-            raise mail2chat.framework.Error("'listeners' items must contain 'address' value")
+            raise framework.Error("'listeners' items must contain 'address' value")
 
         # Ensure listener has an address and port set. The Listener object will validate further upon creation.
         if "port" not in listener.keys():
-            raise mail2chat.framework.Error("'listeners' items must contain 'port' value")
+            raise framework.Error("'listeners' items must contain 'port' value")
 
         # When 'enable_smtps' is set, ensure it is a bool
         if type(listener.get("enable_smtps", ENABLE_SMTPS_DEFAULT)) != bool:
-            raise mail2chat.framework.Error("'enable_smtps' items must be type bool")
+            raise framework.Error("'enable_smtps' items must be type bool")
 
         # Mark this as a TLS enabled listen if SMTPS is enabled
         if listener.get("enable_smtps", ENABLE_SMTPS_DEFAULT):
@@ -195,36 +198,36 @@ def get_listeners_from_dict(config: dict, log_level: int = logging.NOTSET):
 
             # Ensure 'enable_smtps' was not also enabled
             if listener.get("enable_smtps", ENABLE_SMTPS_DEFAULT):
-                raise mail2chat.framework.Error(
+                raise framework.Error(
                     "'listeners' items 'enable_starttls' and 'enable_smtps' cannot be active on the same listener"
                 )
 
             # Ensure it is a bool
             if type(listener.get("enable_starttls", ENABLE_STARTTLS_DEFAULT)) != bool:
-                raise mail2chat.framework.Error("'listeners' item 'enable_starttls' value must be type bool")
+                raise framework.Error("'listeners' item 'enable_starttls' value must be type bool")
 
             # Check for 'require_starttls' option and ensure it is a bool
             if type(listener.get("require_starttls", REQUIRE_STARTTLS_DEFAULT)) != bool:
-                raise mail2chat.framework.Error("'listeners' item 'require_starttls' value must be type bool")
+                raise framework.Error("'listeners' item 'require_starttls' value must be type bool")
 
         # If this was marked as a TLS listener, validate TLS configuration
         if tls_listener:
             # Require the minimum_tls_protocol to be known
             if listener.get("minimum_tls_protocol", TLS_MINIMUM_VERSION_DEFAULT) not in TLS_MINIMUM_VERSION_OPTIONS:
-                raise mail2chat.framework.Error("'listeners' item 'minimum_tls_protocol' value is not known protocol")
+                raise framework.Error("'listeners' item 'minimum_tls_protocol' value is not known protocol")
 
             # Require a TLS certificate and key
             for field in ["tls_cert", "tls_key"]:
                 if field not in listener.keys():
-                    raise mail2chat.framework.Error(f"'listeners' item '{field}' is required to use stmps or starttls")
+                    raise framework.Error(f"'listeners' item '{field}' is required to use stmps or starttls")
 
                 # Ensure field is string
                 if type(listener.get(field)) != str:
-                    raise mail2chat.framework.Error(f"'listeners' item '{field}' value must be type str")
+                    raise framework.Error(f"'listeners' item '{field}' value must be type str")
 
                 # Ensure field is an existing filepath
                 if not pathlib.Path(listener.get(field)).exists():
-                    raise mail2chat.framework.Error(f"'listeners' item '{field}' is not existing file path")
+                    raise framework.Error(f"'listeners' item '{field}' is not existing file path")
                 
             # Create the SSLContext for this listener
             context = ssl.SSLContext(ssl.PROTOCOL_TLS)
@@ -235,7 +238,7 @@ def get_listeners_from_dict(config: dict, log_level: int = logging.NOTSET):
         # Create an SMTPS listener if configured
         if listener.get("enable_smtps", ENABLE_SMTPS_DEFAULT):
             valid_listeners.append(
-                mail2chat.framework.Listener(
+                framework.Listener(
                     mappings=mappings,
                     address=listener.get("address"),
                     port=listener.get("port"),
@@ -248,7 +251,7 @@ def get_listeners_from_dict(config: dict, log_level: int = logging.NOTSET):
         # Create a STARTTLS listener if configured
         elif listener.get("enable_starttls", ENABLE_STARTTLS_DEFAULT):
             valid_listeners.append(
-                mail2chat.framework.Listener(
+                framework.Listener(
                     mappings=mappings,
                     address=listener.get("address"),
                     port=listener.get("port"),
@@ -261,7 +264,7 @@ def get_listeners_from_dict(config: dict, log_level: int = logging.NOTSET):
         # Create normal SMTP listener otherwise
         else:
             valid_listeners.append(
-                mail2chat.framework.Listener(
+                framework.Listener(
                     mappings=mappings,
                     address=listener.get("address"),
                     port=listener.get("port"),
@@ -285,7 +288,7 @@ def generate_tls_certificate(cert_path, key_path):
 
     # Define self-signed certificate
     cert = crypto.X509()
-    cert.get_subject().CN = "mail2chat.example.com"
+    cert.get_subject().CN = "example.com"
     cert.set_serial_number(0)
     cert.gmtime_adj_notBefore(0)
     cert.gmtime_adj_notAfter(365000)
