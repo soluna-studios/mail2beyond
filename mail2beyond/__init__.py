e7ff35b026c9223a2bdb5ffc2a9a635aa8d1c70f
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-08-14 17:32:32 -0600

    ci: adding staging workflows

--- mail2beyond/__init__.py
+++ mail2beyond/__init__.py
@@ -6,5 +6,8 @@ from . import connectors
 from . import parsers
 from . import tools
 
+# Set the version of this package
+__version__ = "0.0.1"
+
 # Don't include tests module in generated documentation.
 __pdoc__ = {"tests": False}
