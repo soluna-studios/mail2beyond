d15550a6cd658f2cf9b19aaa1d6dbfc2ae2dd270
Jared Hendrickson <jaredhendrickson13@gmail.com>
2023-01-24 13:25:29 -0700

    chore: bump project to v1.0.2

--- mail2beyond/__init__.py
+++ mail2beyond/__init__.py
@@ -7,7 +7,7 @@ from . import parsers
 from . import tools
 
 # Set the version of this package
-__version__ = "0.0.1"
+__version__ = "1.0.2"
 
 # Don't include tests module in generated documentation.
 __pdoc__ = {"tests": False}
